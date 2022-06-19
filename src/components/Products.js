de47f6bfc0a0e1ab4043ab86e2d44dd3726b02de
Jiahui Feng <feng_jeffrey@yahoo.com>
2022-06-19 17:43:36 -0400

    MVP complete

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import ProductContext from './../contexts/ProductContext';
 
 // Components
 import Product from './Product';
 
 const Products = props => {
+	const values = useContext(ProductContext);
+	console.log('values', values);
+
 	return (
 		<div className="products-container">
-			{props.products.map(product => (
+			{values.products.map(product => (
 				<Product
 					key={product.id}
 					product={product}
-					addItem={props.addItem}
+
 				/>
 			))}
 		</div>
